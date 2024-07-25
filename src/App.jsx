6ca6817a1bfb1b00be6e7a7d223c7f814bfa853b
Shivani Rajput <shivanirajput0906@gmail.com>
2024-07-25 16:01:56 +0530

    Setup tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-600 underline">
+      Hello evryone, let's make something!
+    </div>
+  );
+}
+
+export default App;

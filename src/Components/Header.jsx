c7e82a861177fe76fe8189b3d32cfdbd3ea9b47d
Shivani Rajput <shivanirajput0906@gmail.com>
2024-07-27 13:57:41 +0530

    Add loggedin user in header and fixed bug also

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,9 +2,11 @@ import { signOut } from "firebase/auth";
 import React from "react";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -22,16 +24,14 @@ const Header = () => {
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-      <div className="flex p-4">
-        <img
-          className="w-11 h-11"
-          src="https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e"
-          alt="user-icon"
-        />
-        <button onClick={handleSignOut} className="text-white font-bold pl-3">
-          Sign Out
-        </button>
-      </div>
+      {user && (
+        <div className="flex p-4">
+          <img className="w-11 h-11" src={user?.photoURL} alt="user-icon" />
+          <button onClick={handleSignOut} className="text-white font-bold pl-3">
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,14 +4,18 @@ import { checkValidData } from "../utils/validations";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const email = useRef(null);
   const password = useRef(null);
@@ -42,8 +46,28 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/92521258?v=4",
+          })
+            .then(() => {
+              // Profile updated!'
+              const { uid, email, displayName, photoURL } = auth.currentUser;
+              dispatch(
+                addUser({
+                  uid: uid,
+                  email: email,
+                  displayName: displayName,
+                  photoURL: photoURL,
+                })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              setErrorMessage(error.message);
+            });
           console.log(user);
-          navigate("/browse");
         })
         .catch((error) => {
           const errorCode = error.code;
